842242fc02c8ccc0201f7e2ba610e52b5adaf219
ImanAlfathanYudha <alfathan.iman@gmail.com>
2026-04-18 23:38:46 +0700

    add services

---
+++ src/app/services/csv-upload.service.ts
@@ -0,0 +1,60 @@
+import { Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { BehaviorSubject, forkJoin, Observable, switchMap, tap } from 'rxjs';
+import { Transaction, TransactionSummary } from '../model/transaction.model';
+
+const API_URL_UPLOAD_TRANSACTION = 'http://localhost:8800/api/v1/transaction/upload';
+const API_URL_GET_TRANSACTION = 'http://localhost:8800/api/v1/transaction/balance'
+const API_URL_GET_ISSUE = 'http://localhost:8800/api/v1/transaction/issues'
+@Injectable({
+  providedIn: 'root'
+})
+export class CsvUploadService {
+
+  constructor(private http: HttpClient) { }
+
+  private transactionsSubject = new BehaviorSubject<Transaction[]>([]);//Subject + punya current value. tempat nyimpen state + alat broadcast ke semua listener. Akan jadi pusat data stream
+  //objek transaction di sini adalah total transaction yang berhasil aja
+  transactions$ = this.transactionsSubject.asObservable();//transactions$ akan jadi data stream. versi read-only dari data (biar component cuma bisa baca)
+
+  private summarySubject = new BehaviorSubject<TransactionSummary>({
+    totalBalance: 0,
+    totalTransactions: 0,
+    totalFailed: 0,
+    totalPending: 0
+  });
+
+  summary$ = this.summarySubject.asObservable();
+
+  //Karena HttpClient di Angular memang mengembalikan Observable. Karena fungsi yang return async result, Observable dipakai untuk handle async flow (HTTP request)
+  uploadTransactionCSV(file: File): Observable<any> {
+    const formData = new FormData();
+    formData.append('file', file);
+
+    return this.http.post(API_URL_UPLOAD_TRANSACTION, formData).pipe(//dikasih pipe agar hasil dari hit endpoint bisa dimodifikasi (Minimal disimpen aja).
+      switchMap(() =>
+        forkJoin({
+          transactions: this.http.get(API_URL_GET_TRANSACTION),
+          issues: this.http.get(API_URL_GET_ISSUE)
+        })
+      ),
+
+      tap((res: any) => {
+        const transactionData = res.transactions?.Data?.transactions || {};
+        const issueData = res.issues?.Data?.issues || [];
+
+        this.transactionsSubject.next(transactionData); //cara update data + ngasih tau semua subscriber kalau datanya berubah. mirip update state (mirip setState)
+
+        const summary: TransactionSummary = {
+          totalTransactions: transactionData?.transactions?.length || 0,
+          totalBalance: transactionData?.total_balance || 0,
+          totalFailed: issueData?.filter((i: any) => i.status === 'FAILED').length,
+          totalPending: issueData?.filter((i: any) => i.status === 'PENDING').length
+        };
+
+        this.summarySubject.next(summary); //cara update data + ngasih tau semua subscriber kalau datanya berubah. mirip update state (mirip setState)
+      })
+    );
+  }
+}
+
