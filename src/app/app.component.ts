0326fb8b4cc8ad79a7f84ae9e129f5b90d902fc1
ImanAlfathanYudha <alfathan.iman@gmail.com>
2026-04-21 00:24:10 +0700

    add styling; add error init

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -9,13 +9,21 @@ import { CsvUploadService } from './services/csv-upload.service';
 export class AppComponent {
   title = 'csv-viewer-app';
   transactions: any[] = [];
-  
-    constructor(private csvService: CsvUploadService) { }
-  
-    ngOnInit() {
-      this.csvService.transactions$.subscribe(data => {
-        console.log('tes table data on app ', data)
-        this.transactions = data || [];
-      });
-    }
+
+  constructor(private csvService: CsvUploadService) { }
+  loading = false;
+  error: string | null = null;
+
+  ngOnInit() {
+    this.csvService.transactions$.subscribe(data => {
+      console.log('tes table data on app ', data)
+      this.transactions = data || [];
+    });
+    this.csvService.loading$.subscribe(val => {
+      this.loading = val;
+    });
+    this.csvService.error$.subscribe(err => {
+      this.error = err;
+    });
+  }
 }
