715b1829611640c128367048069cc61e55f1b5e3
ImanAlfathanYudha <alfathan.iman@gmail.com>
2026-04-24 17:56:51 +0700

    add pagination

--- src/app/components/table-transaction/table-transaction.component.ts
+++ src/app/components/table-transaction/table-transaction.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { Transaction } from 'src/app/model/transaction.model';
 import { CsvUploadService } from 'src/app/services/csv-upload.service';
 
 @Component({
@@ -8,7 +9,8 @@ import { CsvUploadService } from 'src/app/services/csv-upload.service';
 })
 export class TableTransactionComponent {
   transactions: any[] = [];
-
+  pageSize = 5;
+  currentPage = 1;
   constructor(private csvService: CsvUploadService) { }
 
   ngOnInit() {
@@ -17,4 +19,28 @@ export class TableTransactionComponent {
       this.transactions = data;
     });
   }
+
+  get paginatedTransactions(): Transaction[] {
+    const start = (this.currentPage - 1) * this.pageSize;
+    const end = start + this.pageSize;
+
+    return this.transactions.slice(start, end);
+  }
+
+  nextPage() {
+    if (this.currentPage < this.totalPages) {
+      this.currentPage++;
+    }
+  }
+
+  prevPage() {
+    if (this.currentPage > 1) {
+      this.currentPage--;
+    }
+  }
+
+  get totalPages(): number {
+    return Math.ceil(this.transactions.length / this.pageSize);
+  }
 }
+
