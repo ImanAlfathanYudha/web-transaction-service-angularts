66986740a7692a29358ed94d011e8ef4bec78521
ImanAlfathanYudha <alfathan.iman@gmail.com>
2026-04-28 14:33:25 +0700

    Add precomputed pending transaction object

--- src/app/components/pending-transaction/pending-transaction.component.ts
+++ src/app/components/pending-transaction/pending-transaction.component.ts
@@ -9,8 +9,10 @@ import { CsvUploadService } from 'src/app/services/csv-upload.service';
 })
 export class PendingTransactionComponent {
   pendingTransactions: Transaction[] = [];
-
-  constructor(private csvService: CsvUploadService) {}
+  paginatedPendingTransactions: Transaction[] = [];
+  pageSize = 5;
+  currentPage = 1;
+  constructor(private csvService: CsvUploadService) { }
 
   ngOnInit() {
     this.csvService.pendingTransaction$.subscribe(data => {
@@ -18,6 +20,38 @@ export class PendingTransactionComponent {
         t => t.status === 'PENDING'
       );
     });
-    console.log('tes pendingTransactions comp ',this.pendingTransactions)
+    console.log('tes pendingTransactions comp ', this.pendingTransactions)
+    this.paginateTransaction()
+  }
+
+  paginateTransaction() {
+    const start = (this.currentPage - 1) * this.pageSize;
+    const end = start + this.pageSize;
+
+    this.paginatedPendingTransactions = this.pendingTransactions.slice(start, end);
+  }
+
+  nextPage() {
+    if (this.currentPage < this.totalPages) {
+      this.currentPage++;
+      this.paginateTransaction()
+    }
+  }
+
+  prevPage() {
+    if (this.currentPage > 1) {
+      this.currentPage--;
+      this.paginateTransaction()
+    }
+  }
+
+  get totalPages(): number {
+    return Math.ceil(this.pendingTransactions.length / this.pageSize);
+  }
+
+  onPageSizeChange(size: number) {
+    this.pageSize = size;
+    this.currentPage = 1;
+    this.paginateTransaction()
   }
 }
