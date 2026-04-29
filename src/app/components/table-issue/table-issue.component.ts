b5e0052894433a78b204c3053f869cdfb3552aa2
ImanAlfathanYudha <alfathan.iman@gmail.com>
2026-04-29 19:43:02 +0700

    Add pagination mechanism on table issue

--- src/app/components/table-issue/table-issue.component.ts
+++ src/app/components/table-issue/table-issue.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { Transaction } from 'src/app/model/transaction.model';
 import { CsvUploadService } from 'src/app/services/csv-upload.service';
 
 @Component({
@@ -7,14 +8,50 @@ import { CsvUploadService } from 'src/app/services/csv-upload.service';
   styleUrls: ['./table-issue.component.scss']
 })
 export class TableIssueComponent {
-  issues: any[] = [];
+  issuesTransactions: Transaction[] = [];
+  paginatedIssuesTransactions: Transaction[] = [];
+  pageSize = 5;
+  currentPage = 1;
 
   constructor(private csvService: CsvUploadService) { }
 
+
   ngOnInit() {
     this.csvService.issues$.subscribe(data => {
-      console.log('🔥 issue data:', data);
-      this.issues = data;
+      this.issuesTransactions = data
     });
+    console.log('tes pendingTransactions comp ', this.issuesTransactions)
+    this.paginateTransaction()
+  }
+
+  paginateTransaction() {
+    const start = (this.currentPage - 1) * this.pageSize;
+    const end = start + this.pageSize;
+
+    this.paginatedIssuesTransactions = this.issuesTransactions.slice(start, end);
+  }
+
+  nextPage() {
+    if (this.currentPage < this.totalPages) {
+      this.currentPage++;
+      this.paginateTransaction()
+    }
+  }
+
+  prevPage() {
+    if (this.currentPage > 1) {
+      this.currentPage--;
+      this.paginateTransaction()
+    }
+  }
+
+  get totalPages(): number {
+    return Math.ceil(this.issuesTransactions.length / this.pageSize);
+  }
+
+  onPageSizeChange(size: number) {
+    this.pageSize = size;
+    this.currentPage = 1;
+    this.paginateTransaction()
   }
 }
